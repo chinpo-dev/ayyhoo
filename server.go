47458bfb0e834a55689b0cf6b905a32f6453c26a
chinpo-dev <54976721+chinpo-dev@users.noreply.github.com>
2019-09-06 17:48:58 +0300

    Added LIST response to AUTHRESP
    Doesn't work however.

--- server.go
+++ server.go
@@ -40,15 +40,6 @@ func main() {
     }
 }
 
-func ymsg(service byte, status byte, session_id byte) []byte {
-    msg := make([]byte, 20)
-    msg[0], msg[1], msg[2], msg[3] = 89, 77, 83, 71 // YMSG protocol
-    msg[4] = 10 // version 10
-    msg[11] = service
-    msg[15] = status
-    return msg
-}
-
 func decodeData(data []byte) map[string]string {
     dat := make(map[string]string)
     splat := bytes.Split(data, []byte{192,128})
@@ -59,17 +50,30 @@ func decodeData(data []byte) map[string]string {
     return dat
 }
 
-func encodeData(datamap map[string]string) []byte {
+func encodeData(dataarray []string) []byte {
     payload := []byte{}
     sep := []byte{192,128}
-    for key, value := range datamap {
-        entry := []byte(key)
-        entry = append(entry, sep...)
-        entry = append(entry, []byte(value)...)
+    for i := 0 ; i<len(dataarray) ; i++ {
+        entry := []byte(dataarray[i])
         entry = append(entry, sep...)
         payload = append(payload, entry...)
     }
     return payload
+}
+
+func ymsg(service byte, status byte, session_id byte, data []byte) []byte {
+    msg := make([]byte, 20)
+    msg[0], msg[1], msg[2], msg[3] = 89, 77, 83, 71 // YMSG protocol
+    msg[4] = 10 // version 10
+    msg[11] = service
+    msg[15] = status
+    data_s := len(data)
+    if data_s > 0 {
+        msg[8] = byte(data_s / 256)
+        msg[9] = byte(data_s % 256)
+        msg = append(msg, data...)
+    }
+    return msg
 }
 
 func handleRequest(conn net.Conn) {
@@ -101,11 +105,43 @@ func handleRequest(conn net.Conn) {
         message := make([]byte, 20)
         if buf[11] == 76 {
             //handshake
-            message = ymsg(76, 1, 0)
+            message = ymsg(76, 1, 0, []byte{})
         } else if buf[11] == 87 {
             //auth
-            message = ymsg(87, 1, 0)
-            fmt.Println(encodeData(map[string]string{"1":"a"}))
+            // FIXME: actually implement authorization
+            username := datamap["1"]
+            message = ymsg(87, 1, 1, encodeData([]string{"1", username, "94", "AA.BBCCDDEEGGHHIIH_HII--"}))
+        } else if buf[11] == 84 {
+            //authresp
+            // FIXME: actually implement authorization
+            username := datamap["1"]
+            respdata := make([]string, 0)
+            respdata = append(respdata, "87")
+            respdata = append(respdata, "a\x0A")
+            respdata = append(respdata, "88")
+            respdata = append(respdata, "")
+			respdata = append(respdata, "89")
+			respdata = append(respdata, "")
+			respdata = append(respdata, "59")
+			respdata = append(respdata, "Y\x09")
+			respdata = append(respdata, "59")
+			respdata = append(respdata, "T\x09")
+			respdata = append(respdata, "59")
+			respdata = append(respdata, "C\x09mg=1")
+			respdata = append(respdata, "3")
+			respdata = append(respdata, username)
+			respdata = append(respdata, "90")
+			respdata = append(respdata, "1")
+			respdata = append(respdata, "100")
+			respdata = append(respdata, "0")
+			respdata = append(respdata, "101")
+			respdata = append(respdata, "")
+			respdata = append(respdata, "102")
+			respdata = append(respdata, "")
+			respdata = append(respdata, "93")
+			respdata = append(respdata, "86400")
+            message = ymsg(85, 0, 1, encodeData(respdata))
+            // 85: LIST
         }
 
         //message := buf
@@ -113,7 +149,7 @@ func handleRequest(conn net.Conn) {
         _ = n
         fmt.Println(buf)
         fmt.Println(strconv.Itoa(reqLen))
-        fmt.Println("Sent back:")
+        fmt.Printf("Sent back: ")
         fmt.Println(message)
 
         // Write the message in the connection channel.
