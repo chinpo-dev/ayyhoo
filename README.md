0006f03040d51434c9837611c279788b21573162
chinpo-dev <54976721+chinpo-dev@users.noreply.github.com>
2019-09-07 16:05:11 +0300

    Working fake-login
    Managed to make the client believe it is logged in. Needed the LOGON signal(0x01).

--- README.md
+++ README.md
@@ -6,10 +6,15 @@ http://www.oldversion.com/windows/yahoo-messenger-5-5-1228
 
 
 Important docs:  
-http://libyahoo2.sourceforge.net/  
-*whole source code for client, and some documentation for v9 of the protocol I'm trying to implement*  
-https://gitlab.com/valtron/msn-server/wikis/YMSG-Protocol  
-*overview of the development of the protocol and differences in versions*  
+(http://jymsg9.sourceforge.net/)  
+* overall best source for our purposes, implements v9/10 of the protocol in easy to understand code
+(http://libyahoo2.sourceforge.net/)  
+* whole source code for client, and some documentation for v9 of the protocol I'm trying to implement  
+* v9 is implemented in libyahoo2-0.7.0 version
+(https://gitlab.com/valtron/msn-server/wikis/YMSG-Protocol)  
+* overview of the development of the protocol and differences in versions  
+(http://web.archive.org/web/20100924153734/http://www.ycoderscookbook.com/tutorials/)
+* really old-school tutorial originally for exploiting the protocol
 
 ## Usage
 Visit `HKEY_CURRENT_USER\Software\Yahoo\Pager` in Windows Registry, and change `socket server` to your server, and add your IP to the start of `IPLookup`.
--- server.go
+++ server.go
@@ -1,158 +1,156 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "os"
-    "strconv"
-    "bytes"
+	"fmt"
+	"net"
+	"os"
+//	"strconv"
+	"bytes"
+	"bufio"
+	"io"
 )
 
 const (
-    CONN_HOST = ""
-    CONN_PORT = "5050"
-    CONN_TYPE = "tcp"
+	CONN_HOST = ""
+	CONN_PORT = "5050"
+	CONN_TYPE = "tcp"
+	PROTOCOL_VER = 10
 )
 
 func main() {
-    // Listen for incoming connections.
-    l, err := net.Listen(CONN_TYPE, ":"+CONN_PORT)
-    if err != nil {
-        fmt.Println("Error listening:", err.Error())
-        os.Exit(1)
-    }
-    // Close the listener when the application closes.
-    defer l.Close()
-    fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
-    for {
-        // Listen for an incoming connection.
-        conn, err := l.Accept()
-        if err != nil {
-            fmt.Println("Error accepting: ", err.Error())
-            os.Exit(1)
-        }
+	l, err := net.Listen(CONN_TYPE, ":"+CONN_PORT)
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	// Close the listener when the application closes.
+	defer l.Close()
+	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
 
-        //logs an incoming message
-        fmt.Printf("Received message %s -> %s \n", conn.RemoteAddr(), conn.LocalAddr())
+		fmt.Println("Connected:", conn.RemoteAddr())
 
-        // Handle connections in a new goroutine.
-        go handleRequest(conn)
-    }
+		// Handle connections in a new goroutine.
+		go handleRequest(conn)
+	}
 }
 
 func decodeData(data []byte) map[string]string {
-    dat := make(map[string]string)
-    splat := bytes.Split(data, []byte{192,128})
-    for i := 0 ; i<len(splat) -1 ; i+=2 {
-        dat[string(splat[i])] = string(splat[i + 1])
-    }
-    fmt.Println(dat)
-    return dat
+	dat := make(map[string]string)
+	splat := bytes.Split(data, []byte{192,128})
+	for i := 0 ; i<len(splat) -1 ; i+=2 {
+		dat[string(splat[i])] = string(splat[i + 1])
+	}
+	return dat
 }
 
 func encodeData(dataarray []string) []byte {
-    payload := []byte{}
-    sep := []byte{192,128}
-    for i := 0 ; i<len(dataarray) ; i++ {
-        entry := []byte(dataarray[i])
-        entry = append(entry, sep...)
-        payload = append(payload, entry...)
-    }
-    return payload
+	payload := []byte{}
+	sep := []byte{192,128}
+	for i := 0 ; i<len(dataarray) ; i++ {
+		entry := []byte(dataarray[i])
+		entry = append(entry, sep...)
+		payload = append(payload, entry...)
+	}
+	return payload
 }
 
 func ymsg(service byte, status byte, session_id byte, data []byte) []byte {
-    msg := make([]byte, 20)
-    msg[0], msg[1], msg[2], msg[3] = 89, 77, 83, 71 // YMSG protocol
-    msg[4] = 10 // version 10
-    msg[11] = service
-    msg[15] = status
-    data_s := len(data)
-    if data_s > 0 {
-        msg[8] = byte(data_s / 256)
-        msg[9] = byte(data_s % 256)
-        msg = append(msg, data...)
-    }
-    return msg
+	msg := make([]byte, 20)
+	msg[0], msg[1], msg[2], msg[3] = 89, 77, 83, 71 // YMSG protocol
+	msg[4] = PROTOCOL_VER // version 10
+	msg[11] = service
+	msg[15] = status
+	msg[19] = session_id
+	data_s := len(data)
+	if data_s > 0 {
+		msg[8] = byte(data_s / 256)
+		msg[9] = byte(data_s % 256)
+		msg = append(msg, data...)
+	}
+	return msg
 }
 
 func handleRequest(conn net.Conn) {
-    for {
-        buf := make([]byte, 20)
-        reqLen, err := conn.Read(buf)
-        if err != nil {
-            fmt.Println("Error reading:", err.Error())
-            fmt.Println("Closing connection")
-            conn.Close()
-            break
-        }
+	c := bufio.NewReader(conn)
+	for {
+		buf := make([]byte, 20)
+		_, err := io.ReadFull(c, buf)
+		if err != nil {
+			fmt.Println("Error reading:", err.Error())
+			fmt.Println("Closing connection")
+			conn.Close()
+			break
+		}
 
-        data_s := 256*int(buf[8]) + int(buf[9])
-        data := make([]byte, data_s)
-        if data_s > 0 {
-            reqLen, err := conn.Read(data)
-            if err != nil {
-                fmt.Println("Error reading data:", err.Error())
-            }
-            _ = reqLen
-        }
-        datamap := make(map[string]string)
-        if data_s > 0 {
-            datamap = decodeData(data)
-        }
-        _ = datamap
+		data_s := 256*int(buf[8]) + int(buf[9])
+		data := make([]byte, data_s)
+		if data_s > 0 {
+			_, err := io.ReadFull(c,data)
+			if err != nil {
+				fmt.Println("Error reading data:", err.Error())
+			}
+		}
+		datamap := make(map[string]string)
+		if data_s > 0 {
+			datamap = decodeData(data)
+		}
 
-        message := make([]byte, 20)
-        if buf[11] == 76 {
-            //handshake
-            message = ymsg(76, 1, 0, []byte{})
-        } else if buf[11] == 87 {
-            //auth
-            // FIXME: actually implement authorization
-            username := datamap["1"]
-            message = ymsg(87, 1, 1, encodeData([]string{"1", username, "94", "AA.BBCCDDEEGGHHIIH_HII--"}))
-        } else if buf[11] == 84 {
-            //authresp
-            // FIXME: actually implement authorization
-            username := datamap["1"]
-            respdata := make([]string, 0)
-            respdata = append(respdata, "87")
-            respdata = append(respdata, "a\x0A")
-            respdata = append(respdata, "88")
-            respdata = append(respdata, "")
-            respdata = append(respdata, "89")
-            respdata = append(respdata, "")
-            respdata = append(respdata, "59")
-            respdata = append(respdata, "Y\x09")
-            respdata = append(respdata, "59")
-            respdata = append(respdata, "T\x09")
-            respdata = append(respdata, "59")
-            respdata = append(respdata, "C\x09mg=1")
-            respdata = append(respdata, "3")
-            respdata = append(respdata, username)
-            respdata = append(respdata, "90")
-            respdata = append(respdata, "1")
-            respdata = append(respdata, "100")
-            respdata = append(respdata, "0")
-            respdata = append(respdata, "101")
-            respdata = append(respdata, "")
-            respdata = append(respdata, "102")
-            respdata = append(respdata, "")
-            respdata = append(respdata, "93")
-            respdata = append(respdata, "86400")
-            message = ymsg(85, 0, 1, encodeData(respdata))
-            // 85: LIST
-        }
+		message := make([]byte, 20)
+		if buf[11] == 76 {
+			//handshake
+			message = ymsg(76, 1, 1, []byte{})
+		} else if buf[11] == 87 {
+			//auth
+			// FIXME: actually implement authorization
+			username := datamap["1"]
+			message = ymsg(87, 1, 1, encodeData([]string{"1", username,
+				 "94", "AA.BBCCDDEEGGHHIIH_HII--"}))
+		} else if buf[11] == 84 {
+			//authresp
+			// FIXME: actually implement authorization
+			username := datamap["1"]
+			respdata := make([]string, 0)
+			respdata = append(respdata, []string{"87","\x0A"}...)
+			respdata = append(respdata, []string{"88",""}...)
+			respdata = append(respdata, []string{"89",""}...)
+			//respdata = append(respdata, []string{"59","Y\x09"}...)
+			//respdata = append(respdata, []string{"59","T\x09"}...)
+			//respdata = append(respdata, []string{"59","C\x09mg=1"}...)
+			respdata = append(respdata, []string{"3" , username}...)
+			respdata = append(respdata, []string{"90","1"}...)
+			respdata = append(respdata, []string{"100","0"}...)
+			respdata = append(respdata, []string{"101",""}...)
+			respdata = append(respdata, []string{"102",""}...)
+			respdata = append(respdata, []string{"93","86400"}...)
+			message = ymsg(1, 0, 1, encodeData([]string{"7",""}))
+			fmt.Printf("Sent back: ")
+			fmt.Println(message)
+			conn.Write([]byte(message));
+			message = ymsg(85, 0, 1, encodeData(respdata))
+			// 85: LIST
+		} else if buf[11] == 18 {
+			message = ymsg(18, 1, 1, encodeData([]string{}))
+		}
 
-        //message := buf
-        n := bytes.Index(buf, []byte{0})
-        _ = n
-        fmt.Println(buf)
-        fmt.Println(strconv.Itoa(reqLen))
-        fmt.Printf("Sent back: ")
-        fmt.Println(message)
+		//message := buf
+		n := bytes.Index(buf, []byte{0})
+		_ = n
+		fmt.Printf("Received: ")
+		fmt.Println(buf)
+		if data_s > 0 {
+			fmt.Println("Enclosed data: ", datamap)
+		}
+		fmt.Printf("Sent back: ")
+		fmt.Println(message)
+		fmt.Println()
 
-        // Write the message in the connection channel.
-        conn.Write([]byte(message));
-    }
+		// Write the message in the connection channel.
+		conn.Write([]byte(message));
+	}
 }
