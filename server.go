640e294b3a5095bbf54e5a1661b3593f0d95e3ec
name <me@example.com>
2019-12-28 05:45:00 +0900

    implement messages and clean up code to implement go routines for sending messages
    also creates string lists in a more sane manner than just appending to an empty one

--- server.go
+++ server.go
@@ -8,6 +8,7 @@ import (
 	"bytes"
 	"bufio"
 	"io"
+//    _ "github.com/mattn/go-sqlite3"
 )
 
 const (
@@ -17,7 +18,21 @@ const (
 	PROTOCOL_VER = 10
 )
 
+type connection struct {
+    sess int
+    conn net.Conn
+}
+
+var queue []byte
+var usernames map[string]int //username -> session no
+var get_usernames map[int]string //session no -> username
+var connections map[int]net.Conn //session no -> connection
+var sessioncount = 0
+
 func main() {
+    connections = make(map[int]net.Conn)
+    usernames = make(map[string]int)
+    get_usernames = make(map[int]string)
 	l, err := net.Listen(CONN_TYPE, ":"+CONN_PORT)
 	if err != nil {
 		fmt.Println("Error listening:", err.Error())
@@ -27,6 +42,9 @@ func main() {
 	defer l.Close()
 	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
 	for {
+        for k := range connections {
+            fmt.Println(k)
+        }
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting: ", err.Error())
@@ -60,13 +78,13 @@ func encodeData(dataarray []string) []byte {
 	return payload
 }
 
-func ymsg(service byte, status byte, session_id byte, data []byte) []byte {
+func ymsg(service byte, status byte, session_id int, data []byte) []byte {
 	msg := make([]byte, 20)
 	msg[0], msg[1], msg[2], msg[3] = 89, 77, 83, 71 // YMSG protocol
 	msg[4] = PROTOCOL_VER // version 10
 	msg[11] = service
 	msg[15] = status
-	msg[19] = session_id
+	msg[19] = byte(session_id)
 	data_s := len(data)
 	if data_s > 0 {
 		msg[8] = byte(data_s / 256)
@@ -76,7 +94,22 @@ func ymsg(service byte, status byte, session_id byte, data []byte) []byte {
 	return msg
 }
 
+func send(conn net.Conn, service byte, status byte, session_id int, dataarray []string) {
+    msg := ymsg(service, status, session_id, encodeData(dataarray))
+    conn.Write(msg)
+	fmt.Printf("Sent: ")
+	fmt.Println(msg)
+    if len(dataarray) > 0 {
+	    fmt.Printf("Sent data: ")
+	    fmt.Println(dataarray)
+    }
+	fmt.Println()
+}
+
 func handleRequest(conn net.Conn) {
+    sessioncount += 1
+    sess := sessioncount
+    connections[sess] = conn
 	c := bufio.NewReader(conn)
 	for {
 		buf := make([]byte, 20)
@@ -101,44 +134,58 @@ func handleRequest(conn net.Conn) {
 			datamap = decodeData(data)
 		}
 
-		message := make([]byte, 20)
 		if buf[11] == 76 {
 			//handshake
-			message = ymsg(76, 1, 1, []byte{})
+			go send(conn, 76, 1, sess, []string{})
 		} else if buf[11] == 87 {
 			//auth
 			// FIXME: actually implement authorization
 			username := datamap["1"]
-			message = ymsg(87, 1, 1, encodeData([]string{"1", username,
-				 "94", "AA.BBCCDDEEGGHHIIH_HII--"}))
+            usernames[username] = sess
+            get_usernames[sess] = username
+			go send(conn, 87, 1, sess, []string{"1", username,
+				 "94", "AA.BBCCDDEEGGHHIIH_HII--"})
 		} else if buf[11] == 84 {
 			//authresp
 			// FIXME: actually implement authorization
+			// FIXME: send real list of friends
+			// FIXME: send online status of friends
+			go send(conn, 1, 0, 1, []string{"7",""})
 			username := datamap["1"]
-			respdata := make([]string, 0)
-			respdata = append(respdata, []string{"87","\x0A"}...)
-			respdata = append(respdata, []string{"88",""}...)
-			respdata = append(respdata, []string{"89",""}...)
-			//respdata = append(respdata, []string{"59","Y\x09"}...)
-			//respdata = append(respdata, []string{"59","T\x09"}...)
-			//respdata = append(respdata, []string{"59","C\x09mg=1"}...)
-			respdata = append(respdata, []string{"3" , username}...)
-			respdata = append(respdata, []string{"90","1"}...)
-			respdata = append(respdata, []string{"100","0"}...)
-			respdata = append(respdata, []string{"101",""}...)
-			respdata = append(respdata, []string{"102",""}...)
-			respdata = append(respdata, []string{"93","86400"}...)
-			message = ymsg(1, 0, 1, encodeData([]string{"7",""}))
-			fmt.Printf("Sent back: ")
-			fmt.Println(message)
-			conn.Write([]byte(message));
-			message = ymsg(85, 0, 1, encodeData(respdata))
-			// 85: LIST
+			respdata := []string{ "87","(No Group):ab\n",
+			                      "88","",
+			                      "89","",
+			                      "59","C\x09mg=1",
+			                      //"59","Y\x09",
+			                      //"59","T\x09",
+			                      "3" , username,
+			                      "90","1",
+			                      "100","0",
+			                      "101","",
+			                      "102","",
+			                      "93","86400"}
+			go send(conn, 85, 1, sess, respdata) // 85: LIST
 		} else if buf[11] == 18 {
-			message = ymsg(18, 1, 1, encodeData([]string{}))
-		}
+			go send(conn, 18, 1, sess, []string{})
+		} else if buf[11] == 6 {
+            target := datamap["5"]
+            // look up in the map for online usernames and get session number
+            if to_sess, ok := usernames[target]; ok {
+                msgcontent := datamap["14"]
+                if msgcontent[0:14] == "<font face=\"\">" {
+                    msgcontent = msgcontent[14:]
+                }
+			    msgdata := []string{"5",target, // to
+			                        "4",datamap["1"], // from
+			                        "14",msgcontent, // message content
+			                        "63",";0",
+			                        "64","0",
+			                        "97","1"}
+                to_conn := connections[to_sess]
+                go send(to_conn, 6, 1, to_sess, msgdata)
+            }
+        }
 
-		//message := buf
 		n := bytes.Index(buf, []byte{0})
 		_ = n
 		fmt.Printf("Received: ")
@@ -146,11 +193,6 @@ func handleRequest(conn net.Conn) {
 		if data_s > 0 {
 			fmt.Println("Enclosed data: ", datamap)
 		}
-		fmt.Printf("Sent back: ")
-		fmt.Println(message)
 		fmt.Println()
-
-		// Write the message in the connection channel.
-		conn.Write([]byte(message));
 	}
 }
